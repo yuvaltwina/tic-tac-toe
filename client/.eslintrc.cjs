ff017e2d5a94eed190f3adf7c9c05c6d85772b62
eilon <eilonshamir123@gmail.com>
2023-08-24 15:00:48 +0300

    first commit

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'client/tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: { '@typescript-eslint/comma-dangle': 'off' },
+};
---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <div>tic tac toe</div>;
+}
+
+export default App;
